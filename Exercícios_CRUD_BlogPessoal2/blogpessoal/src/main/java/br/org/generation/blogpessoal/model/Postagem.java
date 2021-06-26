89c7f1dbeb850ebd97990aeeea8c0fa2f9897726
Fernando Caruso <luisfernandocaruso95@gmail.com>
2021-06-25 21:02:03 -0300

    Exercícios parte 2 CRUD Spring Bot

---
+++ Exercícios_CRUD_BlogPessoal2/blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,69 @@
+package br.org.generation.blogpessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+
+		@Entity
+		@Table (name = "tb_postagens") //nome da tabela = tb_postagens
+		public class Postagem {
+	
+	
+		@Id //PRIMARY KEY
+		@GeneratedValue(strategy = GenerationType.IDENTITY) //Auto_Increment
+		private long id; //big int
+		
+		@NotNull
+		@Size(min = 5, max = 100)
+		private String titulo;
+		
+		@NotNull
+		@Size(min = 10, max = 600)
+		private String texto; 
+		
+		@Temporal(TemporalType.TIMESTAMP)
+		private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+		
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+
+		public String getTexto() {
+			return texto;
+		}
+
+		public void setTexto(String texto) {
+			this.texto = texto;
+		}
+
+		public Date getDate() {
+			return date;
+		}
+
+		public void setDate(Date date) {
+			this.date = date;
+		}
+	}
